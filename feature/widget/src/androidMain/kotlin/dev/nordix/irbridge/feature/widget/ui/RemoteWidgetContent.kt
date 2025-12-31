6a875f65feb3d4805c5af6c8ea16582f794a39be
vmakarov <vmakarov@nomia.net>
2025-12-31 18:56:04 +0100

    feature: add icon; add translation; bump version; style: rounded corners for widget

--- feature/widget/src/androidMain/kotlin/dev/nordix/irbridge/feature/widget/ui/RemoteWidgetContent.kt
+++ feature/widget/src/androidMain/kotlin/dev/nordix/irbridge/feature/widget/ui/RemoteWidgetContent.kt
@@ -2,20 +2,20 @@ package dev.nordix.irbridge.feature.widget.ui
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.background
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.text.FontFamily
 import androidx.glance.text.FontWeight
-import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import dev.nordix.irbridge.feature.widget.domain.RemoteDialogContract
 import dev.nordix.irbridge.remotes.domain.model.Remote
@@ -33,6 +33,7 @@ internal fun RemoteWidgetContent(
         modifier = GlanceModifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.2f))
+            .cornerRadius(8.dp)
     ) {
         remotes.forEach { r ->
             Column(
